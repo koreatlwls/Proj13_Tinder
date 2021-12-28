c1709f8587009ff85518fb7e98c4111befe4f6ad
ksj <koreatlwls@gmail.com>
2021-12-28 12:03:17 +0900

    first commit

---
+++ app/src/main/java/com/example/proj13_tinder/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.example.proj13_tinder
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import com.google.firebase.auth.FirebaseAuth
+
+class MainActivity : AppCompatActivity() {
+
+    private val auth: FirebaseAuth = FirebaseAuth.getInstance()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        if (auth.currentUser == null) {
+            startActivity(Intent(this, LoginActivity::class.java))
+        }else{
+        }
+    }
+}
